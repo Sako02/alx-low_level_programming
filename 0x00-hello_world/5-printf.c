fdaaacaeec3f051b12b57fc52c402e8865315c44
Zahra El <zahrakhoudri@gmail.com>
2023-03-16 19:35:52 -0700

    Initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+        char str[] = "Hello";
+        int i;
+
+        for (i = 5; i >= 0; i--)
+        {
+                printf("%c", str[i]);
+        }
+        return 0;
+}
