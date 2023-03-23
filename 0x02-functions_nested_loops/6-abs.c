b075e3559f13f03d6ec72bbf4f7e413e3af6b6f7
Zahra El <zahrakhoudri@gmail.com>
2023-03-22 17:54:11 -0700

    Initial commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,15 +4,12 @@
  * @n: function parameters
  *  * Return: Always 0.
  */
-int _abs(int)
+int _abs(int N)
 {
-// If the number is less than
-// zero, then multiply by (-1)
 if (N < 0)
 {
 N = (-1) * N;
-} 
-// Print the absolute value
+}
 _putchar(N);
 return (0);
 }
