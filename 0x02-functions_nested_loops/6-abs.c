046137b9a6c8f7409b4d92d7b1f0b5b465f2f72b
Zahra El <zahrakhoudri@gmail.com>
2023-03-22 17:59:04 -0700

    Initial commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_sign - a function that prints the sign of a number.
- * @n: function parameters
+ * computes the absolute value of an integer.
+ * @n: function parameters N 
  *  * Return: Always 0.
  */
 int _abs(int N)
@@ -11,5 +11,5 @@ if (N < 0)
 N = (-1) * N;
 }
 _putchar(N);
-return (0);
+return (N);
 }
