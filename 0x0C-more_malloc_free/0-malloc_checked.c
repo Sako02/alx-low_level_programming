4424b038a5da31f7896c0ea52518df58d7f29e61
Zahra El <zahrakhoudri@gmail.com>
2023-04-14 18:35:52 -0700

    Allooc Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
  * malloc_checked - allocates memory using malloc
  * @b: bytes allocate
  * Return: pointer
+ * terminates the program with exit status 98.
  */
 void *malloc_checked(unsigned int b)
 {
