50e2cabbc48e8e57d08a369fceab436770428539
Zahra El <zahrakhoudri@gmail.com>
2023-04-17 18:46:41 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the file it was compiled
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
