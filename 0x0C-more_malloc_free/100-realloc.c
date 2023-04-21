06bdd08d8b17c4c69b4d360d9f7e51b6634f3a70
Zahra El <zahrakhoudri@gmail.com>
2023-04-21 14:24:48 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,52 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * _realloc - reallocates a memory block using malloc and free
+ * _realloc -  reallocates a memory block using malloc and free
  * @ptr: pointer
- * @old_size: size of old pointer
- * @new_size: size of new pointer
- * Return: new void pointer or NULL if it fails
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
  */
-
-
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
+	char *clone, *relloc;
 	unsigned int i;
 
-	if (!ptr)
-	{
-		p = malloc(new_size);
-		if (!p)
-			return (NULL);
-		return (p);
-	}
-
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	if (new_size == old_size)
-		return (ptr);
-
-	p = malloc(new_size);
-
-	if (!p)
-		return (NULL);
-
-	if (new_size < old_size)
-		for (; i < new_size; i++)
-			((char *)p)[i] = ((char *)ptr)[i];
+	if (ptr != NULL)
+	clone = ptr;
 	else
-		for (; i < old_size; i++)
-			((char *)p)[i] = ((char *)ptr)[i];
-
+	{ return (malloc(new_size)); }
+	if (new_size == old_size)
+	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{ free(ptr);
+	return (0); }
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+	return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
 	free(ptr);
-
-	return (p);
+return (relloc);
 }
-
