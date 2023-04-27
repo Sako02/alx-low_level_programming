2f63ee5e3260bb954786ad3ca2d2311a6e194729
Sako02 <zahrakhoudri@gmail.com>
2023-04-27 15:57:07 -0700

    lists

---
+++ 0x12-singly_linked_list/1-list_len.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements 
+ * @h: A pointer to the first elements
+ *
+ * Return: The number of nodes in the list_t list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
