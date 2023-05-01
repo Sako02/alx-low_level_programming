9340ba13327d4425308de4028a943d28d8a3d1ae
Sako02 <zahrakhoudri@gmail.com>
2023-04-30 20:02:04 -0700

    round3

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - free a linked list
+ * @head: linked list's head/void no return
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+
+	while (ptr != NULL)
+	{
+		head = ptr;
+		ptr = head->next;
+		free(head->str);
+		free(head);
+	}
+}
