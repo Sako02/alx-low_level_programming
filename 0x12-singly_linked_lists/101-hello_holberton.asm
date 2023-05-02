cf3a689c81976bf57570a0f383d719c636ce4a8e
Sako02 <zahrakhoudri@gmail.com>
2023-05-01 19:27:18 -0700

    hoberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+extern printf
+
+section .data
+	alx:	db "Hello, Holberton", 10, 0
+
+section .text
+	global main
+main:
+	; Write the string to stdout
+	mov rdi, alx
+	xor eax, eax
+	call printf
+
+
+	; Exit program
+	mov eax, 0
