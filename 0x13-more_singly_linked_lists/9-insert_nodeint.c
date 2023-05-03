18b51e0a0e505695b7e3c37ce7c280e8d82a7934
Sako02 <zahrakhoudri@gmail.com>
2023-05-02 17:48:01 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,6 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list
- * 
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
